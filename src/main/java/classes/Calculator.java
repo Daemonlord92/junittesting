9a9bf1b0240ab05dd90c27dd15e94ac87d191864
Matthew Jeshua Martin <matthew-martin@programmer.net>
2024-02-27 11:20:00 -0500

    junit Testing

---
+++ src/main/java/classes/Calculator.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Calculator {
+
+    public Calculator() {}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,31 @@
+import classes.Calculator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculator {
+    public Calculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void trash() {
+        calculator = null;
+    }
+
+    @Test
+    public void CalculatorAdd_ShouldSucceed_ReturnCorrectAnswer() {
+        int result = calculator.add(2, 2);
+        Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    public void CalculatorAdd_ShouldNotSucceed_NotEquals() {
+        int result = calculator.add(2,2);
+        Assertions.assertNotEquals(5,result);
+    }
+}
